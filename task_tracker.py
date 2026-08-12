2d57086a3adeee087ae74371e23d2f1c59ad9942
Omid Zamzami <omidzamzami27@gmail.com>
2026-08-12 11:03:20 +0330

    Add the main body of user input

--- task_tracker.py
+++ task_tracker.py
@@ -1,7 +1,7 @@
 def welcome():
     print("***** Welcome to Task Tracker *****")
     print("You can Add, Update, and Delete tasks.")
-    print("Also you will be able to observe the status on your tasks and mark them as todo, in-progress, or done.")
+    print("Also you will be able to observe the status on your tasks and mark them as todo, in-progress, or done.\n")
 
 def main_menu():
     print("***** Main menu *****")
@@ -9,4 +9,32 @@ def main_menu():
     print("2. Mark task")
     print("3. Add task")
     print("4. Update task")
-    print("5. Delete task")
+    print("5. Delete task\n")
+
+
+welcome()
+while True:
+    main_menu()
+    user_input = None
+    while user_input not in [1, 2, 3, 4, 5]:
+        try:
+            user_input = int(input("Enter your number of choice (1-5): "))
+        except ValueError:
+            print("Oops, Wrong input!")
+    match user_input:
+        case 1:
+            pass
+            #Observe_task()
+        case 2:
+            pass
+            #Mark_task()
+        case 3:
+            pass
+            #Add_task()
+        case 4:
+            pass
+            #Update_task()
+        case 5:
+            pass
+            #Delete_task()
+    break

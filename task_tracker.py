98e1b707085aae475dd00d7395cce72353be4975
Omid Zamzami <omidzamzami27@gmail.com>
2026-08-12 10:35:31 +0330

    Add the welcome and main menu functions

---
+++ task_tracker.py
@@ -0,0 +1,12 @@
+def welcome():
+    print("***** Welcome to Task Tracker *****")
+    print("You can Add, Update, and Delete tasks.")
+    print("Also you will be able to observe the status on your tasks and mark them as todo, in-progress, or done.")
+
+def main_menu():
+    print("***** Main menu *****")
+    print("1. Observe tasks (all, todo, in-progress, done)")
+    print("2. Mark task")
+    print("3. Add task")
+    print("4. Update task")
+    print("5. Delete task")
